7636be3c3ab4c4b09514d64fd65b101991c469e2
SamyWaz2808 <151126385+SamyWaz2808@users.noreply.github.com>
2023-11-17 09:42:21 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
+def copy_matrice(m):
+    nouvmatrice=[]
+    for i in m:
+        nouvmatrice.append(i[:])
+    return nouvmatrice
+    
 def display_map(m,d):
-    L=m
+    L=copy_matrice(m)
     for i in range(len(m)):
         for j in range(len(m[i])):
             L[i][j]=d[m[i][j]]
@@ -27,24 +26,37 @@ def display_map_and_char(m,d,p):
     L[p["y"]][p["x"]]=p["char"]
     return L
 
-
+def affichage (m):
+    for i in m:
+        for j in i:
+            print (j, end=' ')
+        print()
+        
 #print(display_map_and_char(map,dico,personnage))
     
 #Ex 2.3
 
 def update_p(letter,p):
     if letter=="z":
-        p["y"]-=1
+        if not(p["y"]==0):
+            p["y"]-=1
     if letter=="q":
-        p["x"]-=1
+        if not(p["x"]==0):
+            p["x"]-=1
     if letter=="s":
-        p["y"]+=1
+        if not(p["y"]==3):
+            p["y"]+=1
     if letter=="d":
-        p["x"]+=1
+        if not(p["x"]==4):
+            p["x"]+=1
     return p
 
-update_p(input("z,q,s ou d?"), personnage)
-print(display_map_and_char(map, dico, personnage))
+affichage(display_map_and_char(map, dico, personnage))
+
+while True: 
+    personnage = update_p(input("z,q,s ou d?"), personnage)
+    affichage(display_map_and_char(map, dico, personnage))
+
 
 
 
