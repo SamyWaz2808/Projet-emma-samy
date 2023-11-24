a1a157848e69594a5836b502f23eb787c8d2d55f
SamyWaz2808 <151126385+SamyWaz2808@users.noreply.github.com>
2023-11-24 09:53:03 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -1,3 +1,5 @@
+from random import randint
+    
 def copy_matrice(m):
     nouvmatrice=[]
     for i in m:
@@ -21,39 +23,56 @@ def create_perso(depart):
 personnage = create_perso((3, 2))
 #print(personnage)
 
-def display_map_and_char(m,d,p):
+def create_map_and_char(m,d,p):
     L=display_map(m, d)
     L[p["y"]][p["x"]]=p["char"]
     return L
 
-def affichage (m):
+def display(m):
     for i in m:
         for j in i:
-            print (j, end=' ')
+            print (j, end='')
         print()
         
-#print(display_map_and_char(map,dico,personnage))
+#print(create_map_and_char(map,dico,personnage))
     
 #Ex 2.3
 
 def update_p(letter,p,m):
-    if letter=="z":
-        if (p["y"]>0):
+    if (letter=="z") and (p["y"]>0) and (m[p["y"]-1][p["x"]] != '#'):
             p["y"]-=1
-    if letter=="q":
-        if (p["x"]>0):
+    if (letter=="q") and (p["x"]>0) and (m[p["y"]][p["x"]-1] != '#'):
             p["x"]-=1
-    if letter=="s":
-        if (p["y"]<len(m)-1):
+    if (letter=="s") and (p["y"]<len(m)-1) and (m[p["y"]+1][p["x"]] != '#'):
             p["y"]+=1
-    if letter=="d":
-        if (p["x"]<len(m[0])-1):
+    if (letter=="d") and (p["x"]<len(m[0])-1) and (m[p["y"]][p["x"]+1] != '#'):
             p["x"]+=1
     return p
-map_perso=display_map_and_char(map, dico, personnage)
-affichage(map_perso)
+
+
+def create_objects(nombre_objet,m):
+    s=[]
+    while len(s)<nombre_objet:
+        n=randint(0,4)
+        k=randint(0,3)
+        if m[k][n]!='#' :
+            s.append((n,k))
+    return s
+
+
+def create_map_char_and_objects(m,d,p,s):
+    L=create_map_and_char(m,d,p)
+    for i,j in s:
+        L[j][i]="."
+    return L
+    
+
+map_perso=create_map_and_char(map, dico, personnage)
+objects=create_objects(4,map_perso)
+map_perso_objects=create_map_char_and_objects(map, dico, personnage,objects)
+display(map_perso_objects)
 
 while True: 
     personnage = update_p(input("z,q,s ou d?"), personnage,map_perso)
-    map_perso=display_map_and_char(map, dico, personnage)
-    affichage(map_perso)
+    map_perso=create_map_char_and_objects(map, dico, personnage,objects)
+    display(map_perso)
