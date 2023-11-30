72a66d5aaeb5fb416dbf9ed19db5bfa841452d30
SamyWaz2808 <151126385+SamyWaz2808@users.noreply.github.com>
2023-11-30 21:02:00 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -1,13 +1,20 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
-Spyder Editor
+Created on Thu Nov 30 18:47:14 2023
 
-This is a temporary script file.
+@author: emmaa
 """
 
 from random import randint
-from colorama import Back,Fore,Style,init
-init()
+from colorama import Back,Fore,Style
+
+def delete_all_walls(m, pos):
+    x, y = pos
+    for i in range(max(0, x - 1), min(len(m[0]), x + 2)):
+        for j in range(max(0, y - 1), min(len(m), y + 2)):
+            if 0 <= i < len(m[0]) and 0 <= j < len(m):
+                m[j][i] = 0
+
 
 
 def generate_map(size_map,proportion_wall):
@@ -17,12 +24,14 @@ def generate_map(size_map,proportion_wall):
     while len(walls_in_map)<nbr_wall:
         a=randint(0,size_map[0]-1)
         b=randint(0,size_map[1]-1)
-        if (a,b) not in walls_in_map:
+        if (a,b) not in walls_in_map :
             walls_in_map.append((a,b))
     for i,j in walls_in_map:
         L[j][i]=1
     return L
-            
+
+
+
     
 def copy_matrice(m):
     nouvmatrice=[]
@@ -42,10 +51,41 @@ def display_map(m):
         for j in i:
             print (j, end='')
         print()
-        
-map = generate_map((9,5), 0.2)
-dico = {0:' ',1:'#'}
-#print(create_map(map, dico))
+
+def choisir_difficulte():
+    print("choisir niveau de difficulté :")
+    print("1. easy")
+    print("2. bof")
+    print("3. hard")
+
+    while True:
+        try:
+            diff = int(input("(1, 2 ou 3) : "))
+            if 1 <= diff <= 3:
+                break
+            else:
+                print("entrer un nombre entre 1 et 3.")
+        except ValueError:
+            print("entrer un nombre.")
+
+    if diff == 1:
+        size_map = (8, 4) 
+        proportion_wall = 0.1  
+    elif diff == 2:
+        size_map = (14, 8)  
+        proportion_wall = 0.2  
+    else:
+        size_map = (18, 10)  
+        proportion_wall = 0.3  
+
+    return size_map, proportion_wall
+
+size_map, proportion_wall = choisir_difficulte()        
+map = generate_map(size_map, proportion_wall)
+
+
+dico = {0:' ',1:Fore.WHITE + Back.WHITE + Style.BRIGHT +'#' +Style.RESET_ALL}
+
 
 def create_perso(depart):
     return {"char": Fore.YELLOW + Style.BRIGHT +"☺" + Style.RESET_ALL, "x": depart[0], "y": depart[1], "score":0}
@@ -55,8 +95,8 @@ b=randint(0,len(map)-1)
 while map[b][a]==1:
     a=randint(0,len(map[0])-1)
     b=randint(0,len(map)-1)
+
 personnage = create_perso((a, b))
-#print(personnage)
 
 def create_map_and_char(m,d,p):
     L=create_map(m, d)
@@ -68,29 +108,27 @@ def display_map_and_char(m):
         for j in i:
             print (j, end='')
         print()
-        
-#print(create_map_and_char(map,dico,personnage))
-    
-#Ex 2.3
 
-def update_p(letter,p,m):
-    if (letter=="z") and (p["y"]>0) and (m[p["y"]-1][p["x"]] != '#'):
-            p["y"]-=1
-    if (letter=="q") and (p["x"]>0) and (m[p["y"]][p["x"]-1] != '#'):
-            p["x"]-=1
-    if (letter=="s") and (p["y"]<len(m)-1) and (m[p["y"]+1][p["x"]] != '#'):
-            p["y"]+=1
-    if (letter=="d") and (p["x"]<len(m[0])-1) and (m[p["y"]][p["x"]+1] != '#'):
-            p["x"]+=1
+
+def update_p(letter, p, m):
+    if letter == "z" and p["y"] > 0 and m[p["y"] - 1][p["x"]] != Fore.WHITE + Back.WHITE + Style.BRIGHT + '#' + Style.RESET_ALL:
+        p["y"] -= 1
+    if letter == "q" and p["x"] > 0 and m[p["y"]][p["x"] - 1] != Fore.WHITE + Back.WHITE + Style.BRIGHT + '#' + Style.RESET_ALL:
+        p["x"] -= 1
+    if letter == "s" and p["y"] < len(m) - 1 and m[p["y"] + 1][p["x"]] != Fore.WHITE + Back.WHITE + Style.BRIGHT + '#' + Style.RESET_ALL:
+        p["y"] += 1
+    if letter == "d" and p["x"] < len(m[0]) - 1 and m[p["y"]][p["x"] + 1] != Fore.WHITE + Back.WHITE + Style.BRIGHT + '#' + Style.RESET_ALL:
+        p["x"] += 1
+    if letter == "e":
+        delete_all_walls(m, (p["x"], p["y"]))
     return p
-
 
 def create_objects(nombre_objet,m):
     s=[]
     while len(s)<nombre_objet:
         n=randint(0,len(m[0])-1)
         k=randint(0,len(m)-1)
-        if m[k][n]!='#' and ((n,k) not in s) and m[k][n]!='☺':
+        if m[k][n]!=Fore.WHITE + Back.WHITE + Style.BRIGHT +'#' +Style.RESET_ALL and ((n,k) not in s) and m[k][n]!=Fore.YELLOW + Style.BRIGHT +"☺" + Style.RESET_ALL:
             s.append((n,k))
     return s
 
@@ -112,18 +150,57 @@ def update_objects(p,objects):
      if (p['x'],p['y']) in objects:
          p['score']+=1
          objects.remove((p['x'],p['y']))
+         
+def create_finish(m):
+    while True:
+        n=randint(0,len(m[0])-1)
+        k=randint(0,len(m)-1)
+        if m[k][n]!=Fore.WHITE + Back.WHITE + Style.BRIGHT +'#' +Style.RESET_ALL and m[k][n]!=Fore.YELLOW + Style.BRIGHT +"☺" + Style.RESET_ALL and m[k][n]!=Fore.RED + Style.BRIGHT +"⁎"+ Style.RESET_ALL :
+            return (n,k)
+         
+def create_map_char_and_objects_and_finish(m,d,p,s,f):
+    L=create_map_char_and_objects(m,d,p,s)
+    L[f[1]][f[0]]="X"
+    return L
 
-map_perso=create_map_and_char(map, dico, personnage)
-objects=create_objects(6,map_perso)
-map_perso_objects=create_map_char_and_objects(map, dico, personnage,objects)
-display_map_and_char_and_objects(map_perso_objects)
+def display_map_and_char_and_objects_and_finish(m):
+    for i in m:
+        for j in i:
+            print (j, end='')
+        print()
+    print(Fore.CYAN + Style.BRIGHT + "SCORE : %d" % personnage["score"] + Style.RESET_ALL)
 
-while True: 
-    personnage = update_p(input("z(↑),q(←),s(↓) ou d(→)?"), personnage,map_perso)
-    update_objects(personnage,objects)
-    map_perso=create_map_char_and_objects(map, dico, personnage,objects)
-    display_map_and_char_and_objects(map_perso)
+abandon=0
+level=1
+while True:
+    map_perso=create_map_and_char(map, dico, personnage)
+    objects=create_objects(6,map_perso)
+    map_perso_objects=create_map_char_and_objects(map, dico, personnage,objects)
+    finish=create_finish(map_perso_objects)
+    map_perso_objects_finish=create_map_char_and_objects_and_finish(map, dico, personnage,objects,finish)
+    display_map_and_char_and_objects_and_finish(map_perso_objects_finish)
+    print("LEVEL : %d" %level)
+    if level>1:
+        print(Fore.GREEN + Style.BRIGHT+ "Prochain niveau!" + Style.RESET_ALL)
+    while True: 
+        personnage = update_p(input("z(↑),q(←),s(↓) ou d(→) ou e ?"), personnage,map_perso_objects)
+        update_objects(personnage,objects)
+        map_perso_objects_finish=create_map_char_and_objects_and_finish(map, dico, personnage,objects,finish)
+        display_map_and_char_and_objects_and_finish(map_perso_objects_finish)
+        print("LEVEL : %d sur 10" %level)
+        if personnage["x"]==finish[0] and personnage["y"]==finish[1]:
+            if personnage["score"]%6!=0:
+                abandon=1
+            break
     
+    map = generate_map(size_map, proportion_wall)
+    level+=1
+    
+    if level==11 or abandon==1:
+        break
 
-
-
+if abandon==1:
+    print (Fore.GREEN + Style.BRIGHT+ "VOUS AVEZ ABANDONNÉ " + Style.RESET_ALL)
+else:
+    print (Fore.GREEN + Style.BRIGHT+ "BRAVO VOUS AVEZ GAGNÉ " + Style.RESET_ALL)
+    
